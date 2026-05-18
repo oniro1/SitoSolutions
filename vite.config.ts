9bc27dc6ee98ea179e12c2c263256acdd2b78724
Il Tuo Nome <alessiocerroni@outlook.it>
2026-05-18 14:30:40 +0200

    servizi

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -6,6 +7,13 @@ export default defineConfig({
     outDir: 'dist',
     minify: 'esbuild',
     rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        about: fileURLToPath(new URL('./about.html', import.meta.url)),
+        servizi: fileURLToPath(new URL('./servizi.html', import.meta.url)),
+        faq: fileURLToPath(new URL('./faq.html', import.meta.url)),
+        contact: fileURLToPath(new URL('./contact.html', import.meta.url))
+      },
       output: {
         manualChunks: undefined
       }
